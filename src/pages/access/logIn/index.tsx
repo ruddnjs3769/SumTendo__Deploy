091cb559f0a48d68c005cab9e10e1504656978ea
leechanghwi <lih120@naver.com>
2023-06-11 18:35:08 +0900

    layout

--- src/pages/access/logIn/index.tsx
+++ src/pages/access/logIn/index.tsx
@@ -4,7 +4,36 @@ import styles from './index.module.scss'
 export default function LogIn() {
   return (
     <div className={styles.container}>
-      <h1>1</h1>
+      <h1>닌텐도 어카운트</h1>
+      <div className={styles['login-group']}>
+        <form action="">
+          <div>
+            <input type="email" name="email" placeholder="메일 주소/로그인 ID" />
+          </div>
+
+          <div>
+            <input type="password" name="password" placeholder="암호" />
+          </div>
+        </form>
+
+        <div className={styles['loginAndSignup']}>
+          <p>
+            <a href="/access/passwordcheck">
+              <span>암호를 잊어버린 경우</span>
+            </a>
+          </p>
+          <div>
+            <button>로그인</button>
+          </div>
+        </div>
+
+        <div className={styles['signup']}>
+          <p>어카운트가 없는 분은</p>
+          <a href="/access/agesignup">
+            <button>신규 작성</button>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/access/passwordCheck/index.tsx
+++ src/pages/access/passwordCheck/index.tsx
@@ -4,7 +4,24 @@ import styles from './index.module.scss'
 export default function PasswordCheck() {
   return (
     <div className={styles.container}>
-      <h1>1</h1>
+      <h1>닌텐도 어카운트</h1>
+      <div className={styles['passwordChange-group']}>
+        <h1>비밀번호 재설정</h1>
+        <form action="">
+          <div>
+            <input type="email" name="email" placeholder="메일 주소/로그인 ID" />
+          </div>
+
+          <div>
+            <input type="text" name="displayName" placeholder="닉네임" />
+          </div>
+        </form>
+        <div>
+          <div className={styles['changeCheck-button']}>
+            <button>확인</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

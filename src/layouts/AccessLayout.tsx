dbf394e1e34084a80052ecf3eb3d3f50ffa1e3bd
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-06-12 14:45:46 +0900

    docs : layout 구조 변경

--- src/layouts/AccessLayout.tsx
+++ src/layouts/AccessLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 import UserHeader from '@/components/common/UserHeader'
 import Footer from '@/components/common/Footer'
 
-export default function CommonLayout() {
+export default function AccessLayout() {
   return (
     <div>
       <UserHeader />

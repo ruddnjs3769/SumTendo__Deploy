d68de88296d53dda75c4d887f631876a632554cc
leechanghwi <lih120@naver.com>
2023-06-13 13:33:14 +0900

    style: access레이아웃수정

--- src/layouts/AccessLayout.tsx
+++ src/layouts/AccessLayout.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import UserHeader from '@/components/common/UserHeader'
 import Footer from '@/components/common/Footer'
+import styles from './AccessLayout.module.scss'
 
 export default function AccessLayout() {
   return (
     <div>
       <UserHeader />
-      <main>
+      <main className={styles.main}>
         <Outlet />
       </main>
       <Footer />
--- src/pages/access/passwordCheck/index.tsx
+++ src/pages/access/passwordCheck/index.tsx
@@ -4,21 +4,21 @@ import styles from './index.module.scss'
 export default function PasswordCheck() {
   return (
     <div className={styles.container}>
-      <h1>닌텐도 어카운트</h1>
+      <h1 className={styles['subTitle']}>닌텐도 어카운트</h1>
       <div className={styles['passwordChange-group']}>
-        <h1>비밀번호 재설정</h1>
-        <form action="">
-          <div>
-            <input type="email" name="email" placeholder="메일 주소/로그인 ID" />
+        <h1 className={styles['subTitle']}>비밀번호 재설정</h1>
+        <form className={styles['passwordForm']} action="">
+          <div className={styles['inputForm']}>
+            <input className={styles['input-text']} type="email" name="email" placeholder="메일 주소/로그인 ID" />
           </div>
 
-          <div>
-            <input type="text" name="displayName" placeholder="닉네임" />
+          <div className={styles['inputForm']}>
+            <input className={styles['input-text']} type="text" name="displayName" placeholder="닉네임" />
           </div>
         </form>
         <div>
-          <div className={styles['changeCheck-button']}>
-            <button>확인</button>
+          <div className={styles['changeCheck-btn']}>
+            <button className={styles['submit-btn']}>확인</button>
           </div>
         </div>
       </div>

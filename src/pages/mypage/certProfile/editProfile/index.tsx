6a60113b2a1b72991cdb1807859c78ae4a176505
Seongeun Kim <kse3447@gmail.com>
2023-06-15 20:52:46 +0900

    profile-page-htmlcss구조구현_기능구현 진행예정

--- src/pages/mypage/certProfile/editProfile/index.tsx
+++ src/pages/mypage/certProfile/editProfile/index.tsx
@@ -10,14 +10,16 @@ export default function EditProfile() {
         <section className={styles.container__section}>
           <div className={styles.section__inner}>
             <div className={styles.inner__context}>
-              <h1 className={styles.inner__title}>프로필</h1>
+              <h1 className={styles.inner__title}>프로필 수정</h1>
               <hr className={styles.inner__line} />
-              <ol className={styles.inner__profile_list}>
-                <li className={`${styles.inner__list} ${styles.inner__nickname}`}>
-                  <label className={styles.inner__label} htmlFor="displayName">
+            </div>
+            <div className={styles.inner__content}>
+              <ol className={styles.inner__content__lists}>
+                <li className={styles.content__list}>
+                  <label className={styles.content__label} htmlFor="displayName">
                     닉네임
                   </label>
-                  <form className={styles.inner__content}>
+                  <form className={styles.content__form}>
                     <input
                       id="displayName"
                       className={`${styles.inputTag} ${styles.displayName}`}
@@ -28,50 +30,63 @@ export default function EditProfile() {
                     />
                   </form>
                 </li>
-                <li className={`${styles.inner__list} ${styles.inner__email}`}>
-                  <label className={styles.inner__label} htmlFor="displayName">
-                    닉네임
+                <li className={styles.content__list}>
+                  <label className={styles.content__label} htmlFor="email">
+                    이메일
                   </label>
-                  <form className={styles.inner__content}>
+                  <form className={styles.content__form}>
                     <input
-                      id="displayName"
-                      className={`${styles.inputTag} ${styles.displayName}`}
-                      type="text"
-                      name="displayName"
-                      placeholder="displayName"
+                      id="email"
+                      className={`${styles.inputTag} ${styles.email}`}
+                      type="email"
+                      name="email"
+                      placeholder="{고객 이메일 데이터}"
+                      disabled
                       required
                     />
                   </form>
                 </li>
-                <li className={`${styles.inner__list} ${styles.inner__password}`}>
-                  <label className={styles.inner__label} htmlFor="displayName">
-                    닉네임
+                <li className={styles.content__list}>
+                  <label className={styles.content__label} htmlFor="password">
+                    비밀 번호
                   </label>
-                  <form className={styles.inner__content}>
+                  <form className={styles.content__form}>
                     <input
-                      id="displayName"
-                      className={`${styles.inputTag} ${styles.displayName}`}
-                      type="text"
-                      name="displayName"
-                      placeholder="displayName"
+                      id="password"
+                      className={`${styles.inputTag} ${styles.password}`}
+                      type="password"
+                      name="password"
+                      placeholder="password"
                       required
                     />
                   </form>
                 </li>
-                <li className={`${styles.inner__list} ${styles.inner__img}`}>
-                  <label className={styles.inner__label} htmlFor="displayName">
-                    닉네임
+                <li className={`${styles.content__list} ${styles.uploade}`}>
+                  <label className={styles.content__label} htmlFor="uploade">
+                    프로필
+                    <br />
+                    이미지
                   </label>
-                  <form className={styles.inner__content}>
-                    <input
-                      id="displayName"
-                      className={`${styles.inputTag} ${styles.displayName}`}
-                      type="text"
-                      name="displayName"
-                      placeholder="displayName"
-                      required
-                    />
-                  </form>
+                  <div className={styles.content__uploade_form}>
+                    <img className={styles.content__uploade__file} src="" alt="" />
+                    <div className={styles.content__upload_info}>
+                      <span className={styles.upload_text}>프로필 이미지</span>
+                      <div className={styles.upload_subtext}>
+                        - 파일 사이즈 최대 1MB 이하
+                        <br /> - jpg, jpeg, webp, png, gif, svg
+                      </div>
+                      <form className={styles.content__form}>
+                        <input
+                          className={`${styles.inputTag} ${styles.uploade__form}`}
+                          id="uploade"
+                          type="file"
+                          name="file"
+                          accept="image/jpeg, image/png, image/gif, image/svg+xml"
+                          required
+                        />
+                      </form>
+                    </div>
+                  </div>
                 </li>
               </ol>
             </div>

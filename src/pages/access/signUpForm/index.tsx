c172ee528143bbeebc21e5172a762e26d5abcdc2
leechanghwi <lih120@naver.com>
2023-06-19 13:11:19 +0900

    로그인 페이지 기능구현

--- src/pages/access/signUpForm/index.tsx
+++ src/pages/access/signUpForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback, ChangeEvent } from 'react'
 import styles from './index.module.scss'
 import { SignUpRequest } from '@/types/auth'
 
@@ -8,15 +8,26 @@ export default function SignUpForm() {
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [profileImage, setProfileImage] = useState('')
-  const [errorMessages, setErrorMessages] = useState({
-    email: '',
-    displayName: '',
-    password: '',
-    confirmPassword: ''
-  })
-  // email & 비밀번호 정규식
+  const [previewImage, setPreviewImage] = useState('')
+  const [isChecked, setIsChecked] = useState(false)
+
+  // 메세지 상태 저장
+  const [emailMsg, setEmailMsg] = useState('')
+  const [pwdMsg, setPwdMsg] = useState('')
+  const [confirmPwdMsg, setConfirmPwdMsg] = useState('')
+  const [displayNameMsg, setDisplayNameMsg] = useState('')
+  const [displayNameCheckMsg, setDisplayNameCheckMsg] = useState('')
+
+  // 중복체크
+  const [isDisplayNameChecked, setIsDisplayNameChecked] = useState(false)
+  const [isDisplayNameDuplicate, setIsDisplayNameDuplicate] = useState(false)
+
+  // email & 닉네임 & 비밀번호 정규식
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
   const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,20}$/
+  const displayNameRegex = /^[a-zA-Z가-힣0-9\s]{1,10}$/
+
+  // 더미데이터
 
   const dummySignUpRequest: SignUpRequest = {
     email: 'example@example.com',
@@ -25,10 +36,80 @@ export default function SignUpForm() {
     profileImgBase64: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/...'
   }
 
+  // 중복닉네임 실험용 더미데이터
+
+  const dummyData = ['John', 'Jane', 'Alice']
+
+  // 중복체크 기능
+
+  const onIsDisplayNameChecked = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    const isDuplicate = dummyData.includes(displayName)
+
+    // 중복체크 메세지
+
+    if (isDuplicate) {
+      setDisplayNameCheckMsg('이미 사용 중인 닉네임입니다.')
+    } else {
+      setDisplayNameCheckMsg('사용 가능한 닉네임입니다.')
+    }
+
+    setIsDisplayNameDuplicate(isDuplicate)
+    setIsDisplayNameChecked(true)
+  }
+
+  // 유효성 검사기능 및 중복체크 전부 통과해야 폼 제출성공
+  const validateForm = () => {
+    // 이메일 유효성 검사
+    if (!emailRegex.test(email)) {
+      return false
+    }
+
+    // 닉네임 유효성 검사
+    if (!displayNameRegex.test(displayName)) {
+      return false
+    }
+
+    // 중복 체크 확인 여부 검사
+    if (!isDisplayNameChecked || isDisplayNameDuplicate) {
+      return false
+    }
+
+    // 비밀번호 유효성 검사
+    if (!passwordRegex.test(password)) {
+      return false
+    }
+
+    // 비밀번호 일치 여부 검사
+    if (password !== confirmPassword) {
+      return false
+    }
+
+    // 모든 유효성 검사 통과
+    return true
+  }
+  //체크박스 기능
+  const handleCheckboxChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    console.log('체크박스 확인')
+    setIsChecked(e.target.checked)
+  }, [])
+
+  // 회원가입 폼제출 및 회원가입 요청
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
     if (validateForm()) {
+      console.log('폼 제출 성공')
       console.log('회원가입 요청:', {
+        email,
+        password,
+        displayName,
+        profileImgBase64: dummySignUpRequest.profileImgBase64
+      })
+    } else {
+      console.log('폼 제출 실패')
+      console.log('회원가입 요청실패:', {
         email,
         password,
         displayName,
@@ -36,57 +117,86 @@ export default function SignUpForm() {
       })
     }
   }
-  //이미지 업로드
-  const uploadImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files as FileList
-    for (const file of files) {
+
+  // 이메일 콜백함수를 이용해 메세지를 실시간으로 반영
+  const onChangeEmail = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const curEmail = e.target.value
+      setEmail(curEmail)
+
+      if (!emailRegex.test(curEmail)) {
+        setEmailMsg('메일 주소가 올바르지 않습니다.')
+      } else {
+        setEmailMsg('올바른 이메일 형식입니다.')
+      }
+    },
+    [emailRegex]
+  )
+
+  // 닉네임 콜백함수를 이용해 현재 입력값에 따른 메세지를 실시간으로 반영
+  const onChangeDisplayName = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const curDisplayName = e.target.value
+      setDisplayName(curDisplayName)
+
+      if (!displayNameRegex.test(curDisplayName)) {
+        setDisplayNameMsg('닉네임이 올바르지 않습니다.')
+      } else {
+        setDisplayNameMsg('올바른 닉네임 형식입니다.')
+      }
+    },
+    [displayNameRegex]
+  )
+  // 비밀번호 콜백함수를 이용해 현재 입력값에 따른 메세지를 실시간으로 반영
+  const onChangePassword = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const curPassword = e.target.value
+      setPassword(curPassword)
+
+      if (!passwordRegex.test(curPassword)) {
+        setPwdMsg('비밀번호가 올바르지 않습니다.')
+      } else {
+        setPwdMsg('안전한 비밀번호입니다.')
+      }
+    },
+    [passwordRegex]
+  )
+  // 비밀번호 일치 여부 콜백함수를 이용해 현재 입력값에 따른 메세지를 실시간으로 반영
+  const onChangeConfirmPassword = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const curConfirmPassword = e.target.value
+      setConfirmPassword(curConfirmPassword)
+
+      if (password !== curConfirmPassword) {
+        setConfirmPwdMsg('비밀번호가 일치하지 않습니다.')
+      } else {
+        setConfirmPwdMsg('올바른 비밀번호입니다.')
+      }
+    },
+    [password]
+  )
+
+  //이미지 미리보기
+  const handleChangeFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files
+    if (files && files.length > 0) {
+      const file = files[0]
       const reader = new FileReader()
+
+      reader.onloadend = () => {
+        const base64 = reader.result
+        if (base64) {
+          setPreviewImage(base64.toString())
+          uploadImage(base64.toString())
+        }
+      }
+
       reader.readAsDataURL(file)
-      reader.addEventListener('load', async (ev: ProgressEvent<FileReader>) => {
-        setProfileImage(ev.target?.result as string)
-      })
     }
   }
-
-  //회원가입 양식 유효성 검사
-  const validateForm = (): boolean => {
-    const { email, password, displayName } = dummySignUpRequest
-    let isValid = true
-    const updatedErrorMessages = {
-      email: '',
-      displayName: '',
-      password: '',
-      confirmPassword: ''
-    }
-
-    // 이메일 유효성 검사
-
-    if (!emailRegex.test(email)) {
-      updatedErrorMessages.email = '메일 주소가 올바르지 않습니다.'
-      isValid = false
-    }
-
-    // 닉네임 길이 확인
-    if (displayName.length > 10) {
-      updatedErrorMessages.displayName = '닉네임이 올바르지 않습니다.'
-      isValid = false
-    }
-
-    // 비밀번호 길이 및 숫자와 영문 혼합 확인
-
-    if (!passwordRegex.test(password)) {
-      updatedErrorMessages.password = '비밀번호가 올바르지 않습니다.'
-      isValid = false
-    }
-
-    // 비밀번호 일치 여부 확인
-    if (password !== confirmPassword) {
-      updatedErrorMessages.confirmPassword = '비밀번호가 일치하지 않습니다.'
-      isValid = false
-    }
-    // 에러 메시지 업데이트
-    setErrorMessages(updatedErrorMessages)
-    return isValid
+  //프로필 이미지 상태설정
+  const uploadImage = (base64: string) => {
+    setProfileImage(base64)
   }
 
   return (
@@ -101,16 +211,41 @@ export default function SignUpForm() {
               <input
                 className={styles['input-info']}
                 type="text"
-                value={displayName}
                 name="displayName"
-                onChange={(event) => setDisplayName(event.target.value)}
-                onBlur={validateForm}
+                onChange={onChangeDisplayName}
                 placeholder="10자 이내"
                 required
               />
-              {errorMessages.displayName && <div className={styles.error}>{errorMessages.displayName}</div>}
+              {/* 중복체크하기전 메시지 출력 */}
+
+              {displayNameMsg && !isDisplayNameChecked && !isChecked && (
+                <div className={displayNameMsg === '올바른 닉네임 형식입니다.' ? styles.msg : styles.error}>
+                  {displayNameMsg}
+                </div>
+              )}
+
+              {/* 중복체크 후 메시지 출력 */}
+
+              {displayNameCheckMsg && isDisplayNameChecked && !isChecked && (
+                <div className={displayNameCheckMsg === '사용 가능한 닉네임입니다.' ? styles.msg : styles.error}>
+                  {displayNameCheckMsg}
+                </div>
+              )}
+              {/* 중복체크하지 않은 경우 오류 메시지 */}
+
+              {!isDisplayNameChecked && isChecked && <div className={styles.error}>중복 확인을 해주십시오.</div>}
+
+              {/* 중복체크 및 약간동의 후 메시지 */}
+              {isDisplayNameChecked && isChecked && (
+                <div className={displayNameCheckMsg === '사용 가능한 닉네임입니다.' ? styles.msg : styles.error}>
+                  {displayNameCheckMsg}
+                </div>
+              )}
             </div>
-            <button className={styles['duplicate-check']}>중복확인</button>
+
+            <button className={styles['duplicate-check']} onClick={onIsDisplayNameChecked}>
+              중복확인
+            </button>
           </div>
 
           <div className={styles['signup-input-group']}>
@@ -121,12 +256,13 @@ export default function SignUpForm() {
                 type="email"
                 name="email"
                 value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                onBlur={validateForm}
+                onChange={onChangeEmail}
                 placeholder="메일 주소"
                 required
               />
-              {errorMessages.email && <div className={styles.error}>{errorMessages.email}</div>}
+              {emailMsg && (
+                <div className={emailMsg === '올바른 이메일 형식입니다.' ? styles.msg : styles.error}>{emailMsg}</div>
+              )}
             </div>
           </div>
 
@@ -138,13 +274,14 @@ export default function SignUpForm() {
                 type="password"
                 name="password"
                 value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                onBlur={validateForm}
+                onChange={onChangePassword}
                 placeholder="영문과 숫자를 혼합한 8자 이상 20자 미만"
                 autoComplete="off"
                 required
               />
-              {errorMessages.password && <div className={styles.error}>{errorMessages.password}</div>}
+              {pwdMsg && (
+                <div className={pwdMsg === '안전한 비밀번호입니다.' ? styles.msg : styles.error}>{pwdMsg}</div>
+              )}
             </div>
           </div>
 
@@ -156,21 +293,35 @@ export default function SignUpForm() {
                 type="password"
                 name="confirmPassword"
                 value={confirmPassword}
-                onChange={(event) => setConfirmPassword(event.target.value)}
-                onBlur={validateForm}
+                onChange={onChangeConfirmPassword}
                 placeholder="영문과 숫자를 혼합한 8자 이상 20자 미만"
                 autoComplete="off"
                 required
               />
-              {errorMessages.confirmPassword && <div className={styles.error}>{errorMessages.confirmPassword}</div>}
+              {confirmPwdMsg && (
+                <div className={confirmPwdMsg === '올바른 비밀번호입니다.' ? styles.msg : styles.error}>
+                  {confirmPwdMsg}
+                </div>
+              )}
             </div>
           </div>
 
           <div className={styles['image-form']}>
             <div className={styles['image-input']}>
-              <input type="file" onChange={uploadImage} accept="image/jpeg, image/png, image/gif, image/svg+xml" />
-              <div>이미지넣을곳</div>
-              <span>프로필 이미지</span>
+              <div>
+                <input
+                  type="file"
+                  onChange={handleChangeFile}
+                  accept="image/jpeg, image/png, image/gif, image/svg+xml"
+                />
+              </div>
+
+              {!previewImage ? (
+                <div className={styles['image-upload']} />
+              ) : (
+                <img className={styles['preview-image']} src={previewImage} alt="프로필 이미지" />
+              )}
+              <span className={styles['image-title']}>프로필 이미지</span>
             </div>
             <div className={styles['image-info']}>
               <span>
@@ -183,12 +334,25 @@ export default function SignUpForm() {
           </div>
           <div className={styles['form-submit']}>
             <div className={styles['info']}>
-              <input type="checkbox" />
-              <span>닌텐도 어카운트의 이용약관 및 개인정보처리방침에 동의합니다.</span>
+              <label className={styles['checkbox-container']} htmlFor="checkboxId">
+                <input
+                  type="checkbox"
+                  id="checkboxId"
+                  className={styles['checkBox-input']}
+                  checked={isChecked}
+                  onChange={handleCheckboxChange}
+                />
+                <span>닌텐도 어카운트의 이용약관 및 개인정보처리방침에 동의합니다.</span>
+              </label>
             </div>
             <div className={styles['continue-group']}>
-              <button className={styles['continue-button']} type="submit" onClick={handleSubmit}>
-                계속
+              <button
+                className={`${styles['continue-button']} ${isChecked ? styles['continue-button-checked'] : ''}`}
+                type="submit"
+                value="Sign up"
+                disabled={!isChecked}
+              >
+                완료
               </button>
             </div>
           </div>

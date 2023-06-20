1ceb34f53963035f320947b16f51caf9fe91280d
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-06-20 19:28:54 +0900

    :sparkles: feat : BankConnect컴포넌트에서 데이터 받기
    api연동 준비했습니다

--- src/components/payment/payMethod/BankConnect.tsx
+++ src/components/payment/payMethod/BankConnect.tsx
@@ -1,33 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './BankConnect.module.scss'
 import { getBankLogo } from './PossibleBank'
+import { AccountConnectionRequest } from '@/types/account'
 
 interface Props {
   bankName: string
   bankCode: string
   bankDigits: number[]
+  handleBankConnectData: (data: AccountConnectionRequest) => void
 }
 
-export default function BankConnection({ bankName, bankCode, bankDigits }: Props) {
+export default function BankConnection({ bankName, bankCode, bankDigits, handleBankConnectData }: Props) {
   const bankLogo = getBankLogo(bankName)
 
   const [accountNumber, setAccountNumber] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
-
-  /*
-  {
-    "name": "KB국민은행",
-    "code": "004",
-    "digits": [3, 2, 4, 3],
-    "disabled": false
-  },
-*/
-  /*
-  bankCode: string // 연결할 은행 코드 (필수!)
-  accountNumber: string // 연결할 계좌번호 (필수!)
-  phoneNumber: string // 사용자 전화번호 (필수!)
-  signature: boolean // 사용자 서명 (필수!)
-  */
 
   const handleAccountNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
@@ -60,6 +47,16 @@ export default function BankConnection({ bankName, bankCode, bankDigits }: Props
     const formattedValue = inputDigits.slice(0, 11).replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3')
     setPhoneNumber(formattedValue)
   }
+
+  // api 사용에 필요한 requestbody 부모컴포넌트(payMethod 로 전달)
+  useEffect(() => {
+    handleBankConnectData({
+      bankCode: bankCode, // 연결할 은행 코드 (필수!)
+      accountNumber: accountNumber.replace(/-/g, ''), // 연결할 계좌번호 (필수!)
+      phoneNumber: phoneNumber.replace(/-/g, ''), // 사용자 전화번호 (필수!)
+      signature: true // 사용자 서명 (필수!)
+    })
+  }, [phoneNumber, accountNumber])
 
   return (
     <div className={styles.container}>
@@ -105,3 +102,55 @@ export default function BankConnection({ bankName, bankCode, bankDigits }: Props
     </div>
   )
 }
+
+// 2. 계좌 연결
+/*
+### 계좌 연결
+
+- 연결된 계좌 잔액에는 자동으로 기본 '3백만원'이 추가됩니다.
+- 요청하는 계좌번호와 전화번호에는``구분이 없어야 합니다.
+
+`curl https://asia-northeast3-heropy-api.cloudfunctions.net/api/account 
+  \ -X 'POST'
+  \ -H 'Authorization: Bearer <accessToken>'`
+
+요청 데이터 타입 및 예시:
+
+`interface RequestBody {
+  bankCode: string // 연결할 은행 코드 (필수!)
+  accountNumber: string // 연결할 계좌번호 (필수!)
+  phoneNumber: string // 사용자 전화번호 (필수!)
+  signature: boolean // 사용자 서명 (필수!)
+}`
+
+`{
+  "bankCode": "088",
+  "accountNumber": "123456789012",
+  "phoneNumber": "01012345678",
+  "signature": true
+}`
+
+응답 데이터 타입 및 예시:
+
+`interface ResponseValue { // 연결된 계좌 정보
+  id: string // 계좌 ID
+  bankName: string // 은행 이름
+  bankCode: string // 은행 코드
+  accountNumber: string // 계좌 번호
+  balance: number // 계좌 잔액
+}`
+  */
+/*
+  {
+    "name": "KB국민은행",
+    "code": "004",
+    "digits": [3, 2, 4, 3],
+    "disabled": false
+  },
+*/
+/*
+  bankCode: string // 연결할 은행 코드 (필수!)
+  accountNumber: string // 연결할 계좌번호 (필수!)
+  phoneNumber: string // 사용자 전화번호 (필수!)
+  signature: boolean // 사용자 서명 (필수!)
+  */
--- src/pages/payment/payMethod/index.tsx
+++ src/pages/payment/payMethod/index.tsx
@@ -7,22 +7,45 @@ import { AccountsBalance } from '@/types/account'
 import { useNavigate } from 'react-router-dom'
 import Modal from '@/components/common/Modal'
 import dummyAccountsList from '@/pages/payment/dummyAccountsList.json'
-import { Banks } from '@/types/account'
+import { Banks, AccountConnectionRequest } from '@/types/account'
 import PossibleBank from '@/components/payment/payMethod/PossibleBank'
 import BankConnect from '@/components/payment/payMethod/BankConnect'
 import { Product } from '@/types/product'
 
 //사용되는 api
 // 1. 계좌 조회
-// 2. 계좌 연결
-// 3. 제품거래(구매) 신청
+// 3.제품거래(구매) 신청
+/*
+  ### 제품 거래(구매) 신청
+
+- 사용자 전용 API입니다.
+- 거래(구매) 신청시 연결된 계좌에서 결제됩니다.
+- 결제할 계좌(ID)를 꼭 선택해야 합니다.(`계좌 목록 및 잔액 조회`API를 사용하세요)
+- 선택한 계좌의 잔액보다 결제 금액이 크면 결제가 처리되지 않습니다.(에러 반환)
+
+`curl https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/buy 
+  \ -X 'POST'
+  \ -H 'Authorization: Bearer <accessToken>'`
+
+요청 데이터 타입 및 예시:
+
+`interface RequestBody {
+  productId: string // 거래할 제품 ID (필수!)
+  accountId: string // 결제할 사용자 계좌 ID (필수!)
+  reservation?: { // 예약 정보(예약 시스템을 사용하는 경우만 필요)
+    start: string // 예약 시작 시간(ISO)
+    end: string // 예약 종료 시간(ISO)
+  }
+}`
+*/
 // 4. 선택가능한 은행 목록 조회
 
 // todo
 // 1. 총 계산금액 (할인율 포함) - 주문 금액, 할인율, 최종 결제 금액 표기 ✅
 // 2. 새로고침 시 모달 초기화 ✅
 // 3. 모달 계좌 연결 정보 입력 (정규식) ✅
-// 4. 계좌 데이터 연결(중요) -
+// 4. 계좌 데이터 연결(중요) -아아아아아
+// 5. api 불러와서 연동
 
 export default function PayMethod() {
   const { accounts }: AccountsBalance = dummyAccounts
@@ -34,6 +57,12 @@ export default function PayMethod() {
   const [nextModal, setNextModal] = useState(false)
   const [bankIndex, setBankIndex] = useState(0)
   const navigate = useNavigate()
+  const [BankConnectData, setBankConnectData] = useState<AccountConnectionRequest>({
+    bankCode: '',
+    accountNumber: '',
+    phoneNumber: '',
+    signature: true
+  })
 
   //cartItems 할인 계산
   const cartItems = JSON.parse(localStorage.getItem('cart') || '[]')
@@ -77,6 +106,35 @@ export default function PayMethod() {
     setNextModal(true)
     setBankIndex(index)
   }
+  //BankConnect컴포넌트서 가져올 데이터를 위한 Props함수
+  const handleBankConnectData = (data: AccountConnectionRequest) => {
+    setBankConnectData(data)
+  }
+
+  const handleBankConnectOrder = () => {
+    const accountNumLength = selectedAccount.digits.reduce((acc, val) => acc + val, 0)
+    if (BankConnectData.accountNumber.length !== accountNumLength) {
+      alert('올바른 계좌번호를 입력해주세요!')
+    } else if (BankConnectData.phoneNumber.length !== 10) {
+      alert('올바른 전화번호를 입력해주세요!')
+    } else {
+      // 수행할 로직
+      navigate('/payment/:username/orderComplete')
+    }
+    // api 은행 계좌 등록 요청. function(BankConnectData)
+    // 요청 완료 시 api 거래 신청 요청. accountId, productId 만 있으면 됨. function(BankConnectData.accountNumber, )
+    // 근데 productId가 배열데이터가 아님. 아
+    // 반복문 사용해서 productId만큼 요청보내기?
+    // 예외처리 - BankConnectData가 양식에 맞지 않을 경우, 올바른 계좌번호를 or 전화번호를 입력해주세요! alert보내기 ✅
+    // 아 어렵다
+    // 완료 시 navigate('/payment/:username/orderComplete')
+  }
+
+  const handleSelectedBankOrder = () => {
+    // api 거래 신청 요청.
+    // 완료시 navigate('/payment/:username/orderComplete')
+    navigate('/payment/:username/orderComplete')
+  }
 
   const selectedAccount = accountsList[bankIndex]
 
@@ -102,12 +160,7 @@ export default function PayMethod() {
               </div>
             </div>
             {isClicked && (
-              <button
-                className={styles.btn}
-                onClick={() => {
-                  navigate('/payment/:username/orderComplete')
-                }}
-              >
+              <button className={styles.btn} onClick={handleSelectedBankOrder}>
                 선택 계좌로 결제하기
               </button>
             )}
@@ -196,13 +249,9 @@ export default function PayMethod() {
               bankName={selectedAccount.name}
               bankCode={selectedAccount.code}
               bankDigits={selectedAccount.digits}
+              handleBankConnectData={handleBankConnectData}
             />
-            <button
-              className={styles.btn}
-              onClick={() => {
-                navigate('/payment/:username/orderComplete')
-              }}
-            >
+            <button className={styles.btn} onClick={handleBankConnectOrder}>
               계좌 등록 후 바로 결제하기
             </button>
           </div>

2daef57cca125056e54315c08657ea6330fd8e64
KIMGYEONGWON <ruddnjs6651@gmail.com>
2023-06-21 13:56:36 +0900

    :art: docs : 사용payment api 분류

---
+++ src/apis/payment/product/index.ts
@@ -0,0 +1,9 @@
+import api from '@/apis'
+import { ProductDetail } from '@/types/product'
+export async function getProduct(productId: ProductDetail['id']) {
+  const response = await api({
+    method: 'GET',
+    url: `/api/products/${productId}`
+  })
+  return response.data
+}

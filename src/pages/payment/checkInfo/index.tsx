bdc11a2bb92739de4b963941410086e54a26af53
KIMGYEONGWON <ruddnjs6651@gmail.com>
2023-06-21 21:58:29 +0900

    feat : user데이터 연결

--- src/pages/payment/checkInfo/index.tsx
+++ src/pages/payment/checkInfo/index.tsx
@@ -3,16 +3,15 @@ import styles from './index.module.scss'
 import PayProcessFlow from '@/components/payment/PayProcessFlow'
 import ShoppingCart from '@/components/payment/ShoppingCart'
 import Btn from '@/components/payment/Btn'
-import dummyUser from '@/pages/payment/dummyUser.json'
+import { useRecoilValue } from 'recoil'
+import { userState } from '@/recoil/common/userState'
 
 export default function CheckInfo() {
   const [totalValue, setTotalValue] = useState(0)
-
-  //recoilState로 대체하기
-  // localStorage에 저장된 accessToken으로 User정보 fetch? (types : User)
-  // Recoil로 상태관리가 되고 있으면 헤더에서 fetch된 state를 갖다쓰면 되는가?!
-  // 일단 dummyUser.json 을 사용합시다.
-  const user = dummyUser.user
+  const user = useRecoilValue(userState)
+  //recoilState로 대체하기 ✔
+  // localStorage에 저장된 accessToken으로 User정보 fetch? (types : User) ✔
+  // Recoil로 상태관리가 되고 있으면 헤더에서 fetch된 state를 갖다쓰면 되는가?! ✔
 
   const getTotalValue = (value: number) => {
     setTotalValue(value)
@@ -42,7 +41,7 @@ export default function CheckInfo() {
           </div>
         </div>
       </div>
-      <ShoppingCart getTotalValue={getTotalValue} />
+      <ShoppingCart getTotalValue={getTotalValue} user={user} />
       <div className={styles.totals}>
         <div className={styles.mark}>
           <span>합계</span>

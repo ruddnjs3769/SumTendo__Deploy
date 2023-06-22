579451ce4e054bf2e7307a33826077e3e05999ec
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-06-22 15:54:23 +0900

    :sparkles: feat : payment에서 사용할 api추가

--- src/pages/payment/payMethod/index.tsx
+++ src/pages/payment/payMethod/index.tsx
@@ -15,29 +15,6 @@ import { Product } from '@/types/product'
 //사용되는 api
 // 1. 계좌 조회
 // 3.제품거래(구매) 신청
-/*
-  ### 제품 거래(구매) 신청
-
-- 사용자 전용 API입니다.
-- 거래(구매) 신청시 연결된 계좌에서 결제됩니다.
-- 결제할 계좌(ID)를 꼭 선택해야 합니다.(`계좌 목록 및 잔액 조회`API를 사용하세요)
-- 선택한 계좌의 잔액보다 결제 금액이 크면 결제가 처리되지 않습니다.(에러 반환)
-
-`curl https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/buy 
-  \ -X 'POST'
-  \ -H 'Authorization: Bearer <accessToken>'`
-
-요청 데이터 타입 및 예시:
-
-`interface RequestBody {
-  productId: string // 거래할 제품 ID (필수!)
-  accountId: string // 결제할 사용자 계좌 ID (필수!)
-  reservation?: { // 예약 정보(예약 시스템을 사용하는 경우만 필요)
-    start: string // 예약 시작 시간(ISO)
-    end: string // 예약 종료 시간(ISO)
-  }
-}`
-*/
 // 4. 선택가능한 은행 목록 조회
 
 // todo
@@ -48,8 +25,8 @@ import { Product } from '@/types/product'
 // 5. api 불러와서 연동
 
 export default function PayMethod() {
-  const { accounts }: AccountsBalance = dummyAccounts //
-  const accountsList: Banks = dummyAccountsList // api 연결 응답데이터 받기
+  const { accounts }: AccountsBalance = dummyAccounts // api/account/getConnectedAccounts
+  const accountsList: Banks = dummyAccountsList // api/account/getSelectableAccounts
   const [isOpen, setIsOpen] = useState(false)
   const [isClicked, setIsClicked] = useState(false)
   const [activeIndex, setActiveIndex] = useState<number | null>(null)

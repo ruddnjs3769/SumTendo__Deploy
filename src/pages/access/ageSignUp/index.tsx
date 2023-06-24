3b93d29eb12e87f101f18cf01048b4d564124f6a
leechanghwi <lih120@naver.com>
2023-06-24 18:15:38 +0900

    찐수정본

--- src/pages/access/ageSignUp/index.tsx
+++ src/pages/access/ageSignUp/index.tsx
@@ -8,14 +8,18 @@ export default function AgeSignUp() {
       <div className={styles['age']}>
         <div className={styles['thirteenUnder']}>
           <a href="/access/signupform">
-            <img />
-            <span>13세 이하는 이곳</span>
+            <div className={styles['thirteen']}>
+              <img src={process.env.PUBLIC_URL + '/images/access/13young.png'} alt="13young" />
+              <span>13세 이하는 이곳</span>
+            </div>
           </a>
         </div>
         <div className={styles['fourteenPlus']}>
           <a href="/access/signupform">
-            <img />
-            <span>14세 이상은 이곳</span>
+            <div className={styles['fourteen']}>
+              <img src={process.env.PUBLIC_URL + '/images/access/13young.png'} alt="14older" />
+              <span>14세 이상은 이곳</span>
+            </div>
           </a>
         </div>
       </div>

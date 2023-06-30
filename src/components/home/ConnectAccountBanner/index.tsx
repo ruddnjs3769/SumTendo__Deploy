e4a3443e51ea237e7ee89b4a7991da5a06421368
peacepiece7 <peacepiece7@naver.com>
2023-06-30 21:03:40 +0900

    fix : 링크 수정

--- src/components/home/ConnectAccountBanner/index.tsx
+++ src/components/home/ConnectAccountBanner/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './index.module.scss'
+import useUserInfo from '@/hooks/useUserInfo'
 
 export default function ConnectAccountBanner() {
+  const [userInfo] = useUserInfo()
   return (
     <div className={styles.container}>
       <p className={styles.title}>Sumtendo Switch</p>
-      <a className={styles.link} href="/">
+      <a className={styles.link} href={`/user/${userInfo.displayName}/account/addAccount`}>
         <p>{'숨텐도 계좌 연결하기 >'}</p>
       </a>
     </div>
